43bbbac4b1007817fc91396b7c2fa6dcaa5290fd
Serra <pcollars01@iesalbarregas.es>
2025-11-19 18:38:48 +0100

    funcionalidad del caballo terminada

--- src/App.java
+++ src/App.java
@@ -6,4 +6,8 @@ public class App {
     public static void funcion(){
         System.out.println("esto es una funcion");
     }
+
+    public static void funcionCaballo(){
+        System.out.println("El caballo se mueve en L");
+    }
 }
