c575d1f5ab99a14d5d65115ecf393a8a09726bb2
Serra <pcollars01@iesalbarregas.es>
2025-11-19 18:27:44 +0100

    funcion tal

--- src/App.java
+++ src/App.java
@@ -2,4 +2,8 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
     }
+
+    public static void funcion(){
+        System.out.println("esto es una funcion");
+    }
 }
